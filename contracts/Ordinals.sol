39add99246efe582d131cc30d55dba8730594494
smartdev112 <liujin941102@gmail.com>
2023-04-19 03:12:32 +0900

    add signature

--- contracts/Ordinals.sol
+++ contracts/Ordinals.sol
@@ -43,15 +43,59 @@ contract Ordinals is OwnableUpgradeable {
         _caller[caller] = approved;
     }
 
-    function setInscription(address coll, uint256 tokenId, string memory inscriptionId) external {
+    function setInscription(
+        address coll,
+        uint256 tokenId,
+        string memory inscriptionId,
+        bytes memory signature
+    ) external {
         require(bytes(_inscription[coll][tokenId]).length == 0, "DOUBLE");
-        
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(inscriptionId, coll, tokenId)
+        );
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
+        address signer = ecrecover(messageHash, v, r, s);
+        require(signer == msg.sender, "Invalid signature");
+
         if (coll != address(0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB)) {
             IERC721Upgradeable tokenERC721 = IERC721Upgradeable(coll);
-            require(tokenERC721.ownerOf(tokenId) == msg.sender || _caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
+            require(
+                tokenERC721.ownerOf(tokenId) == msg.sender ||
+                    _caller[msg.sender] ||
+                    msg.sender == _admin,
+                "INV_CALLER"
+            );
         } else {
             require(_caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
         }
         _inscription[coll][tokenId] = inscriptionId;
+    }
+
+    function splitSignature(bytes memory signature)
+        public
+        pure
+        returns (
+            uint8 v,
+            bytes32 r,
+            bytes32 s
+        )
+    {
+        require(signature.length == 65, "Invalid signature length");
+
+        assembly {
+            // First 32 bytes are the signature header, skip them
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+
+            // Extract the last byte of the signature header, which contains the value of v
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        // If v is 0 or 1, add 27 to it to get the correct value
+        if (v < 27) {
+            v += 27;
+        }
+
+        return (v, r, s);
     }
 }
--- contracts/TransparentUpgradeableProxy.sol
+++ contracts/TransparentUpgradeableProxy.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (proxy/transparent/TransparentUpgradeableProxy.sol)
-
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
