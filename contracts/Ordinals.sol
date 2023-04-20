53ce6d187486305e19019816200fad0741ed1330
smartdev112 <liujin941102@gmail.com>
2023-04-21 03:55:32 +0900

    fix contract

--- contracts/Ordinals.sol
+++ contracts/Ordinals.sol
@@ -2,38 +2,29 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "./lib/Errors.sol";
 
 contract Ordinals is OwnableUpgradeable {
     address public _admin; // is a mutil sig address when deploy
-    address public _parameterAddr;
 
     mapping(address => bool) public _caller;
     mapping(address => mapping(uint256 => string)) public _inscription;
 
-    function initialize(address admin, address parameterControl) initializer virtual public {
+    function initialize(address admin) public virtual initializer {
         require(admin != Errors.ZERO_ADDR, Errors.INV_ADD);
         //        require(parameterControl != Errors.ZERO_ADDR, Errors.INV_ADD);
-
         _admin = admin;
-        _parameterAddr = parameterControl;
         __Ownable_init();
     }
 
     function changeAdmin(address newAdm) external {
-        require(msg.sender == _admin && newAdm != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
+        require(
+            msg.sender == _admin && newAdm != Errors.ZERO_ADDR,
+            Errors.ONLY_ADMIN_ALLOWED
+        );
 
         if (_admin != newAdm) {
             _admin = newAdm;
-        }
-    }
-
-    function changeParam(address newAdm) external {
-        require(msg.sender == _admin && newAdm != Errors.ZERO_ADDR, Errors.ONLY_ADMIN_ALLOWED);
-
-        if (_parameterAddr != newAdm) {
-            _parameterAddr = newAdm;
         }
     }
 
@@ -51,14 +42,10 @@ contract Ordinals is OwnableUpgradeable {
         require(bytes(_inscription[coll][tokenId]).length == 0, "DOUBLE");
         bytes32 hashedMessage = hashMessage(coll, tokenId, inscriptionId);
         address signer = VerifyMessage(hashedMessage, signature);
-        require(signer == msg.sender, "Invalid signature");
 
-        if (coll != address(0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB)) {
-            IERC721Upgradeable tokenERC721 = IERC721Upgradeable(coll);
-            require(tokenERC721.ownerOf(tokenId) == msg.sender || _caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
-        } else {
-            require(_caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
-        }
+        require(_caller[signer], "Invalid signature");
+        require(_caller[msg.sender] || msg.sender == _admin, "INV_CALLER");
+        
         _inscription[coll][tokenId] = inscriptionId;
     }
 
@@ -98,7 +85,9 @@ contract Ordinals is OwnableUpgradeable {
         uint256 tokenId,
         string memory inscriptionId
     ) public pure returns (bytes32) {
-        bytes32 _hashedMessage = keccak256(abi.encodePacked(coll, tokenId, inscriptionId));
+        bytes32 _hashedMessage = keccak256(
+            abi.encodePacked(coll, tokenId, inscriptionId)
+        );
         return _hashedMessage;
     }
 }
